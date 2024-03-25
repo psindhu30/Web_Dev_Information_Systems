fdeee04189b82a5e041b4c7a42b6d06337c030b5
psindhu30 <160877677+psindhu30@users.noreply.github.com>
2024-03-25 00:26:43 +0000

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", schema);
+
+module.exports = User;
